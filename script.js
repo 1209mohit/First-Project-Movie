af6cbfe7225493ff996ffc5404593258834460a7
1209mohit <130536607+1209mohit@users.noreply.github.com>
2025-02-16 11:25:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional: Add event listener for logging download event
+document.querySelector('.download-btn').addEventListener('click', () => {
+  console.log('Video download initiated.');
+});
